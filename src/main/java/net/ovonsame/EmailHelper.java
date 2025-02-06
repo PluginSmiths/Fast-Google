92e46ca797ac1714ede8edc29b01c83d270dd93d
OVNSAME <162483465+OVNSAME@users.noreply.github.com>
2025-02-06 21:15:09 +0300

    Added

--- src/main/java/net/ovonsame/EmailHelper.java
+++ src/main/java/net/ovonsame/EmailHelper.java
@@ -1,60 +1,72 @@
 package net.ovonsame;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
-import org.checkerframework.checker.nullness.qual.NonNull;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.UserCredentials;
+import com.google.common.io.BaseEncoding;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Session;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
 import java.util.Collections;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EmailHelper {
-    @SuppressWarnings("unused")
-    public static void sendEmail(@NonNull String text, @NonNull String sender, @NonNull String getter, @NonNull String credentials) {
+
+    public static void sendEmail(String to, String subject, String bodyText) throws Exception {
+        InputStream in = new FileInputStream("src/airy-legacy-449915-c4-26d4d75ef9b7.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(in)
+                .createScoped(List.of("https://www.googleapis.com/auth/gmail.send"));
+
+        Gmail service = new Gmail.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), new HttpCredentialsAdapter(credentials))
+                .setApplicationName("FastGoogle")
+                .build();
+
+        MimeMessage email = createEmail(to, subject, bodyText);
+
+        sendMessage(service, "fastgoogle2@airy-legacy-449915-c4.iam.gserviceaccount.com", email);
+    }
+
+    private static MimeMessage createEmail(String to, String subject, String bodyText) throws MessagingException {
+        Properties props = System.getProperties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress("fastgoogle2@airy-legacy-449915-c4.iam.gserviceaccount.com"));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+
+        return email;
+    }
+
+    private static Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws MessagingException, IOException {
+        // Переводим MimeMessage в Base64 строку
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        emailContent.writeTo(byteArrayOutputStream);
+        byte[] emailBytes = byteArrayOutputStream.toByteArray();
+
+        String encodedEmail = BaseEncoding.base64Url().encode(emailBytes);
+
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+
+        return service.users().messages().send(userId, message).execute();
+    }
+
+    public static void main(String[] args) {
         try {
-            HttpTransport httpTransport = new NetHttpTransport();
-            JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
-
-            InputStream in = EmailHelper.class.getClassLoader().getResourceAsStream(credentials);
-            if (in == null) throw new FileNotFoundException("File " + credentials + " was not found");
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(in));
-
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(),
-                    jsonFactory,
-                    clientSecrets,
-                    Collections.singletonList("https://www.googleapis.com/auth/gmail.send")
-            ).setAccessType("offline").build();
-
-            Credential credential = new Credential(Credential.AccessMethod);
-
-            Gmail emailService = new Gmail.Builder(httpTransport, jsonFactory, credential)
-                    .setApplicationName("FastGoogle")
-                    .build();
-
-            String emailContent = "From: " + sender + "\n" +
-                    "To: " + getter + "\n" +
-                    "Subject: FastGoogle\n\n" +
-                    text;
-
-            String encodedEmail = Base64.getUrlEncoder().encodeToString(emailContent.getBytes(StandardCharsets.UTF_8));
-
-            Message message = new Message();
-            message.setRaw(encodedEmail);
-
-            emailService.users().messages().send(sender, message).execute();
-            System.out.println("Письмо успешно отправлено!");
+            sendEmail("aroslavkopejcenko@gmail.com", "Test Subject", "This is the email body text.");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/net/ovonsame/Main.java
+++ src/main/java/net/ovonsame/Main.java
@@ -2,6 +2,6 @@ package net.ovonsame;
 
 public class Main {
     public static void main(String[] args) {
-        EmailHelper.sendEmail("Hello", "ovonsame@gmail.com", "aroslavkopejcenko@gmail.com", "credentials.json");
+
     }
 }
